3135877a092953f5163c49b1bc965c189dcb319c
Jaime Barbosa <jaimebs@gmail.com>
2020-05-27 16:11:47 -0300

    feat: first commit.

---
+++ perimeter.js
@@ -0,0 +1,64 @@
+function initMap() {
+  let btnPronto = document.getElementById('btnPronto');
+  let btnPolygon = document.getElementById('btnPolygon');
+  let paths = [];
+  let latitude = 0;
+  let longitude = 0;
+
+  function getLocation() {
+    if (navigator.geolocation) {
+      navigator.geolocation.getCurrentPosition(showPosition);
+    }
+  }
+
+  function showPosition(position) {
+    latitude = position.coords.latitude;
+    longitude = position.coords.longitude;
+    
+    map.setCenter({lat: latitude, lng: longitude})
+  }
+
+  var map = new google.maps.Map(document.getElementById('map'), {
+    // center: {lat: latitude, lng: longitude},
+    zoom: 13
+  });
+
+  var drawingManager = new google.maps.drawing.DrawingManager({
+    // drawingMode: google.maps.drawing.OverlayType.MAKER,
+    drawingControl: false,
+    drawingControlOptions: {
+      position: google.maps.ControlPosition.TOP_CENTER,
+      drawingModes: ['polygon']
+    },
+  });
+  drawingManager.setMap(map);
+
+  // google.maps.event.addListener(drawingManager, 'overlaycomplete', function(e) {
+  //   console.log(e.type);
+  //   console.log(e.getPath().getArray().toString());
+    
+  // })
+
+  google.maps.event.addListener(drawingManager, 'polygoncomplete', function (polygon) {
+    var coordinates = (polygon.getPath().getArray().toString());
+    console.log(coordinates);
+    paths.push(coordinates)
+
+    showArray();
+  });
+
+  btnPronto.addEventListener('click', ()=>{
+    drawingManager.setDrawingMode(null);
+  })
+
+  btnPolygon.addEventListener('click', ()=>{
+    drawingManager.setDrawingMode(google.maps.drawing.OverlayType.POLYGON);
+  })
+
+  function showArray(){
+    console.log(paths);
+    
+  }
+
+  getLocation();
+}
